1eabe024a5eb5d71b6220d7f9f894babef84b2bb
vtsgh <101072785+vtsgh@users.noreply.github.com>
2022-10-04 19:54:34 -0400

    Add files via upload

---
+++ check.js
@@ -0,0 +1,28 @@
+const name = document.getElementById('name')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+form.addEventListener('submit', (e) => {
+  let messages = []
+  if (name.value === '' || name.value == null) {
+    messages.push('Name is required')
+  }
+
+  if (password.value.length <= 8) {
+    messages.push('Password must be longer than 8 characters')
+  }
+
+  if (password.value.length >= 16) {
+    messages.push('Password must be less than 16 characters')
+  }
+
+  if (password.value === 'password') {
+    messages.push('Password cannot be password')
+  }
+
+  if (messages.length > 0) {
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ')
+  }
+})
